7ba53c3a8b3bb5a48ce846199618d57b7a57c0b6
roberto <roberto.scinocca@gmail.com>
2014-05-08 15:38:30 +0200

    update

---
+++ forum/includes/register.php
@@ -0,0 +1,84 @@
+<h2 class="page_header">Aiuto</h2>
+<p>Compilando questo modulo di pre-iscrizione è possibile velocizzare le procedure di accesso ai servizi del Sistema Bibliotecario.<br>
+    Una volta compilato il modulo ...<!--sarà comunque necessario recarsi presso la biblioteca preferita per completare la procedura di iscrizione--><br>
+    Usa questo modulo solo se non ti sei mai iscritto.<br>
+    Non usare questo modulo se hai smarrito login e password. In questo caso rivolgiti alla tua biblioteca di riferimento.<br>
+I campi contrassegnati con un asterisco (*) sono obbligatori.</p>
+<form action="" method="post">
+    <h4 class="page_header">Dati account</h4>
+    <div class="input_field">
+        <div class="row">
+            <div class="colum">
+                <p>Username<sup>*</sup></p>
+                <input type="text" class="input_form">
+            </div>
+            <div class="colum">
+                <p>Email<sup>*</sup></p>
+                <input type="text" class="input_form">
+            </div>
+        </div>
+        <div class="row">
+            <div class="colum">
+                <p>Password<sup>*</sup></p>
+                <input type="text" class="input_form">
+            </div>
+            <div class="colum">
+                <p>Conferma Password<sup>*</sup></p>
+                <input type="text" class="input_form">
+            </div>
+        </div>
+    </div>
+    <h4 class="page_header">Informazioni anagrafiche</h4>
+    <div class="input_field">
+        <div class="row">
+            <div class="colum">
+                <p>Nome</p>
+                <input type="text" class="input_form">
+            </div>
+            <div class="colum">
+                <p>Cognome</p>
+                <input type="text" class="input_form">
+            </div>
+        </div>
+        <div class="row">
+            <div class="colum">
+                <p>Password</p>
+                <input type="text" class="input_form">
+            </div>
+            <div class="colum">
+                <p>Conferma Password</p>
+                <input type="text" class="input_form">
+            </div>
+        </div>
+    </div>
+    <h4 class="page_header">Informazioni legali</h4>
+    <div class="input_field">
+        <div class="row">
+            <div class="colum">
+                
+            </div>
+        </div>
+    </div>
+    <h4 class="page_header">Indirizzo</h4>
+    <div class="input_field">
+        <div class="row">
+            <div class="colum">
+                
+            </div>
+        </div>
+    </div>
+    <h4 class="page_header">Numero di telefono</h4>
+    <div class="input_field">
+        <div class="row">
+            <div class="colum">
+                
+            </div>
+        </div>
+    </div>
+    <div class="input_field">
+        <input type="checkbox" class="input_checkbox">
+        <p class="input_label">Dichiaro di aver letto le condizioni sulla Privacy</p>
+        <sup class="input_label">*</sup>
+    </div>
+    <button>Registrati</button>
+</form>
