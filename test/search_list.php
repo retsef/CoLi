bcfb585bb169d215ab811a3140b256bef9011c46
roberto <roberto.scinocca@gmail.com>
2014-05-30 22:46:51 +0200

    bugfix: ora non ci sono differenze di styling se si visita il sito da firefox o da chrome

---
+++ test/search_list.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <ol>
+            <li>
+                <ul>
+                    <li>Ciao pippo</li>
+                    <li>Frollini production</li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li>Bibbia</li>
+                    <li>Gesu cristo</li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li>Guida intergalattica per autostoppisti</li>
+                    <li>DONT PANIC</li>
+                </ul>
+            </li>
+        </ol>
+    </body>
+</html>
