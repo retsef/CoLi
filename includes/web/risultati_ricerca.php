9f1ddd7c3a3e84d8b6325de447d13efcb875427b
roberto <roberto.scinocca@gmail.com>
2014-06-09 01:40:09 +0200

    aggiunta la barra di navigazione dinamica alla ricerca

--- includes/web/risultati_ricerca.php
+++ includes/web/risultati_ricerca.php
@@ -4,6 +4,8 @@ error_reporting(E_ALL | E_STRICT);
 
 include(ROOT_PATH. "/includes/api/sbn/sbn_manager.php");
 
+$Get_url = "?page=" .$_GET['page'] . "&search=" .$_GET['search'];
+
 $sbn = new sbn_manager();
 try {
     $sbn->sbn_search($_GET['search']);
@@ -14,19 +16,26 @@ try {
 $hits = $sbn->get_hits();
 
 if ($hits) {
-    echo "Found ".$hits ." hits:<br>";
+    echo "<br>";
 
-$start=1;
-$end=5;
+if(isset($_GET['s']))
+    $start=$_GET['s'];
+else
+    $start = 1;
+$step=20;
+$end=$start+$step;
 
-echo "<ol>";
+$pre=$start-$step;
+$last=$hits-$step;
+
+echo "<ol start=".$start.">";
 for($i=$start;$i<=$end;$i++){
     echo 
     "<li>
         <ul>";
     echo "<li>";
         print($sbn->get_Result($i));
-    echo "</li>";
+    echo "</li><br>";
     echo 
     "    </ul>
     </li>";
@@ -35,29 +44,30 @@ echo "</ol>";
 
 echo 
 "<div class=nav_bar_page>"
-    ."<ul>"
-        . "<li>"
-            . "<a href=" ."". "><<</a>"
+    ."<ul>";
+if($start!=1){
+echo "<li>"
+            . "<a href=" .$Get_url ."&s=1". "><<</a>"
         . "</li>"
         . "<li>"
-            . "<a href=" ."". "> < Prec.</a>"
-        . "</li>"
-        . "<li>"
+            . "<a href=" .$Get_url ."&s=".$pre. "> < Prec.</a>"
+        . "</li>";
+}
+echo "<li>"
             . "<a href=" ."". ">"
-            . "Visualizzati" .$start. " - " .$end. " di " .$hits. ""
+            . "Visualizzati " .$start. " - " .$end. " di " .$hits. ""
             . "</a>"
+        . "</li>";
+if($start!=$last) {
+echo "<li>"
+            . "<a href=" .$Get_url ."&s=".$end. ">Succ. ></a>"
         . "</li>"
         . "<li>"
-            . "<a href=" ."". ">Succ. ></a>"
-        . "</li>"
-        . "<li>"
-            . "<a href=" ."". ">>></a>"
-        . "</li>"
-    ."</ul>"
+            . "<a href=" .$Get_url ."&s=".$last. ">>></a>"
+        . "</li>";
+}
+echo "</ul>"
 ."</div>";
-        
-        
-        
 
 } else 
     echo "No records found.";
