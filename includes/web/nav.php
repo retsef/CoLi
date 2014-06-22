bc4c657da6194fafa03e12cccfa6c897fc5308d0
roberto <roberto.scinocca@gmail.com>
2014-06-22 23:25:01 +0200

    aggiunto l'attivazione della linguetta in .nav_bar se si sta sulla pagina che lo riguarda

--- includes/web/nav.php
+++ includes/web/nav.php
@@ -1,8 +1,22 @@
 <div class="nav_bar">
     <ul>
-        <li><a href='./index.php'>Home</a></li>
-        <li><a href='#news_page'>Avvisi Utenti</a></li>
-        <li><a href='#catalog_page'>Percorsi & cataloghi</a></li>
-        <li><a href='#info_page'>Info OPAC</a></li>
+        <li <?php
+        if($_GET['page']=="") {
+            ?>
+        class="selected"
+        <?php
+        }
+                ?>>
+            <a href='./index.php'>Home</a>
+        </li>
+        <li>
+            <a href='#news_page'>Avvisi Utenti</a>
+        </li>
+        <li>
+            <a href='#catalog_page'>Percorsi & cataloghi</a>
+        </li>
+        <li>
+            <a href='#info_page'>Info OPAC</a>
+        </li>
     </ul>
 </div>
