3bc510b96e89ec5b596bd23f7934dde03a65c431
roberto <roberto.scinocca@gmail.com>
2014-06-22 23:52:58 +0200

    Aggiunto style per table; bugfix

--- includes/web/home.php
+++ includes/web/home.php
@@ -1,69 +1,72 @@
 
-<h3>Benvenuto nel catalogo online di Sassinoro</h3>
+<h2 class="page_header">Benvenuto nel catalogo online di Sassinoro</h2>
 <p>Qui potrai consultare i libri disponibili nella Biblioteca Comunale, prenotarli per prelevarli in sede e donarci libri che possono essere utilizzati per aumentare il bagaglio Bibliotecario.</p>
 <p>La Biblioteca Comunale rispetta i seguenti orari:</p>
-<table>
-    <tr>
-        <td>
-            Lunedì
-        </td>
-        <td>
-            9:00
-        </td>
-        <td>
-            13:00
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Martedì
-        </td>
-        <td>
-            15:00
-        </td>
-        <td>
-            18:00
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Mercoledì
-        </td>
-        <td>
-            9:00
-        </td>
-        <td>
-            13:00
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Giovedì
-        </td>
-        <td>
-            15:00
-        </td>
-        <td>
-            18:00
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Venerdì
-        </td>
-        <td>
-            9:00
-        </td>
-        <td>
-            13:00
-        </td>
-    </tr>
+<table cellspacing='0'>
+    <thead>
+        <tr>
+            <th>Giorni della settimana</th>
+            <th>Mattina</th>
+            <th>Pomeriggio</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                Lunedì
+            </td>
+            <td>
+                9:00
+            </td>
+            <td>
+                13:00
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Martedì
+            </td>
+            <td>
+                15:00
+            </td>
+            <td>
+                18:00
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Mercoledì
+            </td>
+            <td>
+                9:00
+            </td>
+            <td>
+                13:00
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Giovedì
+            </td>
+            <td>
+                15:00
+            </td>
+            <td>
+                18:00
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Venerdì
+            </td>
+            <td>
+                9:00
+            </td>
+            <td>
+                13:00
+            </td>
+        </tr>
+    </tbody>
 </table>
-<img style="
-    max-width: 50%; 
-    position: absolute; 
-    float: right; 
-    padding-left: 35%;
-    z-index: -2000;" 
-    src="/CoLi/resources/images/Brainstorming.png">
+<img id="brainstorming" src="/CoLi/resources/images/Brainstorming.png">
 
